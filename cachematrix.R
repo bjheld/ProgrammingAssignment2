bd67df3830a5524e671ce3e82e9bb78757e8d792
bjheld <brettheld@yahoo.com>
2014-08-23 09:28:30 -0400

    Changes made August 23rd

--- cachematrix.R
+++ cachematrix.R
@@ -2,7 +2,11 @@
 ## functions do
 
 ## makeCacheMatrix sets up some functions that are subsequently called by the function cacheSolve
-## August 22, 2014 
+##  "m" is set to Null every time makeCacheMatrix is called
+##  get returns the value of the original (non-inverse) matrix
+##  setsolve is called by cacheSolve...this creates the matrix's inverse and does a superassignment (so it does not need to be recalculated)
+##  getsolve returns the superassigned value to cacheSolve 
+##  Written - August 23, 2014 
 
 makeCacheMatrix <- function(x = matrix()) {
 
@@ -23,7 +27,11 @@ m <- NULL
 
 ## This function actually creates the inverse of the Matrix (if it has not been created before) and 
 ## makes a permanent copy so it does not have to be re-caculated each time.
-## This returns the inverse matrix (last object declared) - August 22, 2014
+## The x$getsolve gets the superassgined inverse matrix m (if it exists)
+## If it ALREADY exists, the if logic prints "getting cached inverse matrix" and just returns the m (without recalculating)
+## If it does NOT exist, the x$get function gets the original matrix, puts it in the solve(data...) function, and puts it in m
+## THEN it uses x$setsolve to superassign the m (inversed matrix), so it now does not need to be recalculated
+## Written - August 23, 2014
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
